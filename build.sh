b82e2e290d254f940e3ad6cd918b44677dd6adb2
Carey Gumaer <gumaerc@mit.edu>
2020-12-11 16:53:53 -0500

    add pdfjs as a submodule and ocw-course-hugo-theme as a hugo module

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git submodule update --init --recursive --remote
-cd themes/ocw-course-hugo-theme
+git submodule update --init --recursive
 yarn install
-npm run build
+hugo mod get -u
+hugo mod vendor
+VENDOR_PATH="_vendor/github.com/mitodl/ocw-course-hugo-theme"
+PDFJS_PATH="pdfjs"
+npm install --prefix $PDFJS_PATH
+npm run gulp:pdfjs
+yarn install --cwd $VENDOR_PATH
+npm run build:webpack --prefix $VENDOR_PATH
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mitodl/standalone-course-prototype
+
+go 1.13
+
+require github.com/mitodl/ocw-course-hugo-theme v0.0.0-20201211214311-c5c91e9293e1 // indirect
