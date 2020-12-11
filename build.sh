b0d7eab6d674aba360ad7ffc9a6a2747d748ab47
Carey Gumaer <gumaerc@mit.edu>
2020-12-11 16:55:48 -0500

    this should be called THEME_PATH

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ git submodule update --init --recursive
 yarn install
 hugo mod get -u
 hugo mod vendor
-VENDOR_PATH="_vendor/github.com/mitodl/ocw-course-hugo-theme"
+THEME_PATH="_vendor/github.com/mitodl/ocw-course-hugo-theme"
 PDFJS_PATH="pdfjs"
 npm install --prefix $PDFJS_PATH
 npm run gulp:pdfjs
-yarn install --cwd $VENDOR_PATH
-npm run build:webpack --prefix $VENDOR_PATH
+yarn install --cwd $THEME_PATH
+npm run build:webpack --prefix $THEME_PATH
