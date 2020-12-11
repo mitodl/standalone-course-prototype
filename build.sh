c9686319be7f446eeb99e891c0f2929295887e6d
Carey Gumaer <gumaerc@mit.edu>
2020-12-11 17:06:03 -0500

    copy build artifacts in place

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 git submodule update --init --recursive
 yarn install
+rm -rf _vendor
 hugo mod get -u
 hugo mod vendor
 THEME_PATH="_vendor/github.com/mitodl/ocw-course-hugo-theme"
@@ -10,3 +11,6 @@ npm install --prefix $PDFJS_PATH
 npm run gulp:pdfjs
 yarn install --cwd $THEME_PATH
 npm run build:webpack --prefix $THEME_PATH
+mkdir -p static/pdfjs
+cp -r pdfjs/build/generic/* static/pdfjs
+cp -r $THEME_PATH/dist/* $THEME_PATH/static
